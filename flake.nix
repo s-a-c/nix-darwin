4f55cd7095112ebe746c464925e481f3f08f1d9b
StandAloneComplex <71233932+s-a-c@users.noreply.github.com>
2024-11-01 16:43:37 +0000

    jetbrains-toolbox

--- flake.nix
+++ flake.nix
@@ -228,6 +228,7 @@
           "insync"
           "itermai"
           "jellyfin" "jellyfin-media-player"
+          "jetbrains-gat" "jetbrains-space" "jetbrains-toolbox"
           "jupyterlab"
           "karabiner-elements"
           "keycast"
